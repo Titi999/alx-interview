426791cff58988ec3d0d975d3848d025b844fd0e
Abubakari Maltiti Bilal <abubakaribilal99@gmail.com>
2022-04-07 12:59:01 +0100

    corrections

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,18 @@
-#!/usr/bin/python3
-from math import factorial
-
-
 def pascal_triangle(n):
-    p = []
-    pascal = []
     if n <= 0:
         return []
-    else:
-        for i in range(n):
-            for j in range(i + 1):
-                p.append(factorial(i) // (factorial(j) * factorial(i - j)))
-            pascal.append(p)
-            p = []
-        return pascal
+
+    pascal = [0] * n
+
+    for i in range(n):
+        new = [0] * (i + 1)
+        new[0] = 1
+        new[len(new) - 1] = 1
+
+        for j in range(1, i):
+            if 0 < j < len(new):
+                new[j] = pascal[i - 1][j] + pascal[i - 1][j - 1]
+
+        pascal[i] = new
+
+    return pascal
