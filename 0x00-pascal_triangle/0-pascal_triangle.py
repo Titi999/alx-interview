c62ad4ad5212588231c1377895e09b36781914dc
Abubakari Maltiti Bilal <abubakaribilal99@gmail.com>
2022-04-07 21:20:17 +0100

    corrections1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,18 +2,13 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-
     pascal = [0] * n
-
     for i in range(n):
         new = [0] * (i + 1)
         new[0] = 1
         new[len(new) - 1] = 1
-
         for j in range(1, i):
             if 0 < j < len(new):
                 new[j] = pascal[i - 1][j] + pascal[i - 1][j - 1]
-
         pascal[i] = new
-
     return pascal
