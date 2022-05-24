10655f6c1f79b257741e762f4dc45f7b4e951843
Abubakari Maltiti Bilal <abubakaribilal99@gmail.com>
2022-05-24 03:45:33 +0100

    comment function

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+A function to rotate n X n matrix
+It should not return anything
+"""
+
+
 def rotate_2d_matrix(matrix):
     rotate = []
     for i in range(0, len(matrix)):
